d31fe9ddd2567af6ac0a5d89a641fd1c04292d66
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-06 21:06:33 +0600

    complete image upload to imagebb

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,22 @@
 import { Link } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
+import axios from 'axios';
+import { imageUpload } from '../../api/utils';
+import useAuth from '../../hooks/useAuth';
 
 const SignUp = () => {
+  const {createUser,signInWithGoogle,updateUserProfile} = useAuth();
+  const handleSubmit = async (event) =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email= form.email.value;
+    const password = form.password.value;
+    const image = form.image.files[0];
+    const imageData = await imageUpload(image);
+    
+
+  }
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -9,7 +24,7 @@ const SignUp = () => {
           <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
           <p className='text-sm text-gray-400'>Welcome to StayVista</p>
         </div>
-        <form
+        <form onSubmit={handleSubmit}
           noValidate=''
           action=''
           className='space-y-6 ng-untouched ng-pristine ng-valid'
