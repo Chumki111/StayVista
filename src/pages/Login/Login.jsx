511c53a5d3fe12e732c799e0556ac1e41deb7696
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-07 13:28:22 +0600

    complete login page with authentication

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,54 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
+import useAuth from '../../hooks/useAuth';
+import { getToken, saveUser } from '../../api/auth';
+import toast from 'react-hot-toast';
+import { ImSpinner9 } from 'react-icons/im';
 
 const Login = () => {
+  const {signIn, signInWithGoogle, loading } = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    
+    try {
+     
+      // registration
+      const result = await signIn(email, password);
+      
+      // get token
+      await getToken(result?.user?.email);
+      navigate('/');
+      toast.success('Sign In Successful')
+
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.message)
+    }
+  }
+  const handleGoogleSignIn =async() =>{
+    try {
+    //  registration with google
+      const result = await signInWithGoogle();
+      
+      // save user data in database
+      const dbResponse = await saveUser(result?.user);
+      console.log(dbResponse);
+      // get token
+      await getToken(result?.user?.email);
+      navigate('/');
+      toast.success('Sign In Successful')
+
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.message)
+    }
+  }
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -11,7 +58,7 @@ const Login = () => {
             Sign in to access your account
           </p>
         </div>
-        <form
+        <form onSubmit={handleSubmit}
           noValidate=''
           action=''
           className='space-y-6 ng-untouched ng-pristine ng-valid'
@@ -54,7 +101,7 @@ const Login = () => {
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              Continue
+              {loading ? <ImSpinner9 className='animate-spin m-auto text-xl'/> : 'Continue'}
             </button>
           </div>
         </form>
@@ -70,13 +117,13 @@ const Login = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <div onClick={handleGoogleSignIn} className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
         </div>
         <p className='px-6 text-sm text-center text-gray-400'>
-          Don&apos;t have an account yet?{' '}
+          Don&apos;t have an account yet?
           <Link
             to='/signup'
             className='hover:underline hover:text-rose-500 text-gray-600'
