7331df2233e2a8eeb9b81b6c41e86bf24173a834
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-07 13:18:25 +0600

    complete logout functionally

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,19 +1,24 @@
 import axiosSecure from "."
 
 // save user data in database
-export const saveUser = async user =>{
-    const currentUser ={
-        email:user?.email,
-        role:'guest',
-        status:'Verified'
+export const saveUser = async user => {
+    const currentUser = {
+        email: user?.email,
+        role: 'guest',
+        status: 'Verified'
     }
-    const {data} = await axiosSecure.put(`/users/${user?.email}`,currentUser);
+    const { data } = await axiosSecure.put(`/users/${user?.email}`, currentUser);
     return data;
 }
 
 // get token from server
-export const getToken = async email =>{
-    const {data} = await axiosSecure.post('/jwt',email);
-    console.log('token received from server-->',data);
+export const getToken = async email => {
+    const { data } = await axiosSecure.post('/jwt', email);
+    console.log('token received from server-->', data);
+    return data;
+}
+// clear token from browser or logout
+export const clearCookie = async () => {
+    const { data } = await axiosSecure.get('/logout');
     return data;
 }
