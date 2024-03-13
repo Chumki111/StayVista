3c80e51176105988b583dbfdaf8d4e156c1679d3
Chumki khatun <chumkikhatun370@gmail.com>
2024-03-14 05:42:44 +0600

    open modal in reserve button

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -2,9 +2,17 @@ import { useState } from "react"
 import Button from "../Button/Button"
 import Calender from "./Calender"
 import { formatDistance} from "date-fns";
+import BookingModal from "../Modal/BookingModal";
+import useAuth from "../../hooks/useAuth";
 
 
 function RoomReservation({roomData}) {
+  const {user} = useAuth();
+
+  let [isOpen, setIsOpen] = useState(false);
+  const closeModal=()=>{
+    setIsOpen(false);
+  }
   const [value, setValue] = useState({
     startDate: new Date(roomData?.from),
     endDate: new Date(roomData?.to),
@@ -16,7 +24,23 @@ function RoomReservation({roomData}) {
     formatDistance(new Date(roomData?.to), new Date(roomData?.from)).split(' ')[0]
   )
   // Total Price Calculation
-  const totalPrice = totalDays * roomData?.price
+  const totalPrice = totalDays * roomData?.price;
+
+  const[bookingInfo,setBookingInfo] = useState({
+    guest:{
+      name:user?.displayName,
+      email:user?.email,
+      image:user?.photoURL
+    },
+    host:roomData?.host?.email,
+    location:roomData?.location,
+    price:totalPrice,
+    to:value?.endDate,
+    from:value?.startDate,
+    title:roomData?.title,
+    roomId:roomData?._id,
+    image:roomData?.image
+  })
   
 
   return (
@@ -31,13 +55,18 @@ function RoomReservation({roomData}) {
         </div>
         <hr />
         <div className="p-4">
-          <Button label={'Reserve'}/>
+          <Button onClick={() =>setIsOpen(true)} label={'Reserve'}/>
         </div>
         <hr />
         <div className="p-4 flex justify-between items-center text-lg font-semibold">
           <div>Total</div>
           <div>$ {totalPrice}</div>
         </div>
+        <BookingModal 
+        closeModal={closeModal}
+         isOpen={isOpen} 
+         bookingInfo={bookingInfo}
+         />
     </div>
   )
 }
